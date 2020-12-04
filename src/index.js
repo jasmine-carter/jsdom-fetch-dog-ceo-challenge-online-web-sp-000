ac37f9a2891e82dd32e0a889b55e23b9fa060fc4
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-04 23:34:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+//iterate through fetched images and add them to doggo div
+function renderImages(json) {
+  const dogImageDiv = document.querySelector("#dog-image-container");
+  const myArray = json.message;
+  for (const element of myArray) {
+    let img = document.createElement("IMG");
+     img.src = element;
+    dogImageDiv.appendChild(img);
+  }
+}
+
+//retrieve random dog images
+function fetchDoggos() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json)  {
+    //iterate through json.messages
+    renderImages(json);
+  });
+}
+
+function fetchBreeds() {
+  fetch("https://dog.ceo/api/breeds/list/all")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    //console.log(json.message)
+    //let testObject = json.message
+    parseBreeds(json);
+  })
+
+}
+
+//take breeds, put them into an array
+function parseBreeds(json) {
+  const dogUl = document.querySelector("#dog-breeds");
+  const myObject = json.message;
+  for (const key in myObject) {
+      let li = document.createElement("li")
+      li.textContent = key
+      dogUl.appendChild(li)
+    }
+  }
+
+//on page load, get random dog images
+//on page load fetch all the dog breeds returned
+document.addEventListener("DOMContentLoaded", () => {
+  fetchDoggos();
+  fetchBreeds();
+})
